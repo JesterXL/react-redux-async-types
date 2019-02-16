e5cacd2a3bd2aab93cb776673a47c74d361f4220
Jesse Warden <jesterxl@jessewarden.com>
2019-02-16 17:34:35 -0500

    wip

--- src/foodsReducer.js
+++ src/foodsReducer.js
@@ -1,15 +1,12 @@
-export const LOAD_FOODS = 'LOAD_FOODS'
-export const LOAD_FOODS_SUCCESS = 'LOAD_FOODS_SUCCESS'
-export const LOAD_FOODS_FAILURE = 'LOAD_FOODS_FAILURE'
+const { union, derivations } = require('folktale/adt/union')
 
-export const loadFoods = () =>
-    ({ type: LOAD_FOODS })
-
-export const loadFoodsSuccess = foods =>
-    ({ type: LOAD_FOODS_SUCCESS, foods })
-
-export const loadFoodsFailure = () =>
-    ({ type: LOAD_FOODS_FAILURE })
+const FoodsState = union('FoodsState', {
+    FoodsLoading() { return {} }
+    , FoodsLoaded(foods) { return { foods } }
+    , FoodsError(error) { return { error } }
+})
+.derive(derivations.debugRepresentation)
+const { FoodsLoading, FoodsLoaded, FoodsError } = FoodsState
 
 const defaultState = {
     isLoading: true
